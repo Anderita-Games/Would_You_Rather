57c97dadcc95c6de57d3ba68eaccfe99992669b2
Andrew Friedman <andrewfriedman01@gmail.com>
2018-05-29 00:04:32 -0400

    Added App
    This is the first version of Would You Rather that GitHub has ever seen!

---
+++ Assets/Generator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Generator : MonoBehaviour {
+	public UnityEngine.UI.Text Line;
+	public GameObject Instructions;
+	public TextAsset Lines;
+
+	void Start () {
+		Instructions.SetActive(true);
+	}
+
+	public void Generation () {
+		if (Instructions.activeSelf) {
+			Instructions.SetActive(false);
+		}
+		StreamReader SR = new StreamReader(new MemoryStream(Lines.bytes));
+		int Random_Number = Random.Range(1,224);
+		for (int i = 1; i <= Random_Number; i++) {
+			Line.text = SR.ReadLine();
+		}
+		SR.Close();
+	}
+}
---
+++ Assets/Legitness.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+import System.IO;
+
+var Hate_Text : UnityEngine.UI.Text;
+var Instructions : GameObject;
+var Phrases : TextAsset;
+
+function Start () {
+	Instructions.SetActive(true);
+}
+
+function Generation () {
+	Instructions.SetActive(false);
+	var SR : StreamReader = new StreamReader(new MemoryStream(Phrases.bytes));
+	var Number_Random : int = Random.Range(1, 271);
+	while (Number_Random > 0) {
+		Hate_Text.text = SR.ReadLine();
+		Number_Random--;
+	}
+	Hate_Text.text = SR.ReadLine();
+	SR.Close();
+}
